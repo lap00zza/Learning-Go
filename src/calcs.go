13071d4ff55a69ef4ba5c0c2c2d19214f97d4c11
Lapoozza <jewelmahanta@gmail.com>
2017-06-19 14:27:43 +0530

    ref (*): added type conversion

--- src/calcs.go
+++ src/calcs.go
@@ -11,6 +11,10 @@ func main ()  {
 
 	fmt.Println("type of a: ", reflect.TypeOf(a), " | type of b: ", reflect.TypeOf(b))
 
-	c := a + b
+	// Should result in invalid operation: a + b (mismatched types float64 and int)
+	// c := a + b
+	// So, lets convert the type of a to an int
+	c := int(a) + b
 	fmt.Println("value of c: ", c, " | type of c: ", reflect.TypeOf(c))
+
 }
