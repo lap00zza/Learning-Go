01ed3b36785a3b48340548833114f8bbf9a1fcbb
Lapoozza <jewelmahanta@gmail.com>
2017-06-21 14:50:44 +0530

    feat (concept): loops

---
+++ src/loops.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// This is how a for statement looks
+	// ForStmt = "for" [ Condition | ForClause | RangeClause ] Block .
+
+	// This loop uses Condition. In this case it is a infinite loop
+	// with the condition set to True
+	/*for {
+		fmt.Println("Infinite!")
+	}*/
+
+	// This loop uses ForClause
+	// This loop contains a simple pre statement, a boolean expression
+	// and a simple post statement
+	// ForClause = [ InitStmt ] ";" [ Condition ] ";" [ PostStmt ] .
+	fmt.Println("Boolean Expression")
+	for i := 0; i < 4; i++ {
+		fmt.Println(i)
+	}
+
+	// This loop uses RangeClause
+	fmt.Println("\nRange")
+	nums := [4]int{1, 2, 3, 4}
+	for _, val := range nums {
+		fmt.Println(val)
+	}
+}
