c4cf233f62264030ecffc13db25487714a77bbd1
Lapoozza <jewelmahanta@gmail.com>
2017-06-21 22:22:57 +0530

    feat (concept, slices): slices under the hood

---
+++ 6. arrays and slices/slices.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	mySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(mySlice[4])
+
+	mySlice[4] = 99
+	fmt.Println(mySlice)
+
+	// When we use [0:2] syntax, in [1,2,3,4,5] we will get
+	// [1,2,3]. It means slice from 0 to 2. 0 is included
+	// and 2 is excluded in the final slice.
+	// Remember:
+	// [:4] -> means 0 to 4
+	// [4:] -> means 4 to length of array - 1
+	sliceOfSlice := mySlice[2:5]
+	fmt.Println(sliceOfSlice)
+}
