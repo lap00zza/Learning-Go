f629ee3f5bbf8094da57a53c20bd13761d9f9fb7
Lapoozza <jewelmahanta@gmail.com>
2017-06-21 13:34:15 +0530

    feat (concept, if): learned about simple statements

--- src/cond_if.go
+++ src/cond_if.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	firstRank := 39
-	secondRank := 100
+	//firstRank := 39
+	//secondRank := 100
 
-	if firstRank < secondRank {
+	// go also support simple statements in if.
+	// IfStmt = "if" [ SimpleStmt ";" ] Expression Block [ "else" ( IfStmt | Block ) ] .
+	// Simple statements: https://golang.org/ref/spec#SimpleStmt
+	if firstRank, secondRank := 39, 100; firstRank < secondRank {
 		fmt.Println("\nFirst is doint better!")
 	} else if firstRank > secondRank {
 		fmt.Println("\nSecond is doing better!")
