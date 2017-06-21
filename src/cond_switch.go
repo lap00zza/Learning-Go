6efd372367f79ec6421b3ba11044441146714f9f
Lapoozza <jewelmahanta@gmail.com>
2017-06-21 13:56:33 +0530

    feat (concept, switch): fallthrough

--- src/cond_switch.go
+++ src/cond_switch.go
@@ -12,11 +12,21 @@ func main() {
 	switch course := "docker"; course {
 	case "linux":
 		fmt.Println("\nHere are some linux courses ...")
+
 	case "docker":
 		fmt.Println("\nHere are some docker courses ...")
+
+		// This keyword allows fallthrough behaviour.
+		// So in this case, after matching case docker,
+		// it will execute the docker block and then
+		// execute the windows block too.
+		fallthrough
+
 	case "windows":
 		fmt.Println("\nHere are some windows courses ...")
+
 	default:
 		fmt.Println("\nSorry! We didn't find a match.")
+
 	}
 }
