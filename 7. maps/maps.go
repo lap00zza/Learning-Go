869191feadeb1d540380fe5dc3c5d3d3069266c9
Lapoozza <jewelmahanta@gmail.com>
2017-06-22 15:36:59 +0530

    feat (concept): learned the basics of maps

---
+++ 7. maps/maps.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Maps are similar to arrays/slices but:
+	// 1. they are unordered
+	// 2. they are <key>:<value> pairs
+	// 3. they are dynamically resizeable
+	// 4. they are references
+	//
+	// +-- https://golang.org/ref/spec#Map_types --+
+	// A map is an unordered group of elements of one type, called the
+	// element type, indexed by a set of unique keys of another type,
+	// called the key type. The value of an uninitialized map is nil.
+	//
+	// MapType     = "map" "[" KeyType "]" ElementType .
+	// KeyType     = Type .
+	//
+	// The comparison operators == and != must be fully defined for
+	// operands of the key type; thus the key type must not be a
+	// function, map, or slice. If the key type is an interface type,
+	// these comparison operators must be defined for the dynamic key
+	// values; failure will cause a run-time panic.
+
+	leagueTitles := make(map[string]int)
+	leagueTitles["Sunderland"] = 6
+	leagueTitles["Newcastle"] = 4
+
+	recentHead2Head := map[string]int{
+		"Sunderland": 5,
+		"Newcastle":  0,
+	}
+	fmt.Println(leagueTitles, recentHead2Head)
+}
